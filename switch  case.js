2d64b978c74537adc82d20c35086d31cef66891c
MZ COMPUTER <MZ COMPUTER@DESKTOP-C5DGRCH>
2022-12-06 14:02:05 +0500

    committed

---
+++ switch  case.js
@@ -0,0 +1,15 @@
+var place='first';
+switch(place){
+    case 'first':
+    console.log('gold');
+
+    break;
+    case 'second':
+        console.log('silver');
+        break;
+    case 'third':
+        console.log(copper);
+        break;
+        default:
+            console.log('no medal')
+}
