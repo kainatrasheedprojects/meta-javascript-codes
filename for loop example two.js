07ee3046d23ecc65bcd28e7384cd8a3a698d39c7
MZ COMPUTER <MZ COMPUTER@DESKTOP-C5DGRCH>
2022-12-06 14:31:56 +0500

    committed

---
+++ for loop example two.js
@@ -0,0 +1,6 @@
+//for loop happy new year
+for(var i=10;i>0; i--)
+{
+    console.log(i);
+}
+console.log('happy new year');
---
+++ while loop.js
@@ -0,0 +1,6 @@
+var counter=3;
+while (counter>0){
+    console.log(counter);
+    counter=counter-1;
+}
+console.log('Happy New Year!');
